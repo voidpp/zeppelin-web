a079ab8d2015cca1b8df465cd6920e274136415a
voidpp <santa.lajos@coldline.hu>
2014-01-10 17:29:29 +0000

    Advanced config parsing. Handle path param in rpc config.

---
+++ wwwroot/js/config.js
@@ -0,0 +1,27 @@
+function parseConfig(p_data)
+{
+	Map.init(p_data, {
+		rpc: {
+			host: window.location.host
+		}
+	});
+	
+	var parseURL = function(p_parts) 
+	{
+		var res = '';
+		res += Map.def(p_parts, 'protocol', 'http') + '://';
+		
+		res += p_parts.host;
+		
+		if(p_parts.port)
+			res += ':' + p_parts.port;
+			
+		res += Map.def(p_parts, 'path', '/');
+		
+		return res;
+	}
+	
+	p_data.rpc.url = parseURL(p_data.rpc);
+	
+	return p_data;
+}
